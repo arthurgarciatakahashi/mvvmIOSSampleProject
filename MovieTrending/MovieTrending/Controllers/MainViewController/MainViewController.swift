1b2a9cb8bb68f57263dd6cfd718e3da05b756c72
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2024-04-19 18:49:39 -0300

    create main xib

--- MovieTrending/MovieTrending/Controllers/MainViewController/MainViewController.swift
+++ MovieTrending/MovieTrending/Controllers/MainViewController/MainViewController.swift
@@ -9,23 +9,12 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // IBoutlets
+     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         self.title = "Primeira View"
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
