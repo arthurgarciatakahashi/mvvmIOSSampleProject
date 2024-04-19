c219fa8b20427348f2f9f63676cf3a1d0ea4c87f
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2024-04-19 15:44:10 -0300

    start architecture

---
+++ MovieTrending/MovieTrending/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MovieTrending
+//
+//  Created by arthur takahashi on 19/04/24.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = MainViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
+        return true
+    }
+}
+
