b7c0ecdae09d0271040f79eb7afe8404fedddce5
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2024-04-20 19:52:47 -0300

    add main movie cell

---
+++ MovieTrending/MovieTrending/Extensions/UIViewExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewExtensions.swift
+//  MovieTrending
+//
+//  Created by arthur takahashi on 20/04/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func round(_ radius: CGFloat = 10) {
+        self.layer.cornerRadius = radius
+        self.clipsToBounds = true
+    }
+    
+    func addBorder(color: UIColor, width: CGFloat) {
+        self.layer.borderWidth = width
+        self.layer.borderColor = color.cgColor
+    }
+}
---
+++ MovieTrending/MovieTrending/View/MainMovieCell/MainMovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MainMovieCell.swift
+//  MovieTrending
+//
+//  Created by arthur takahashi on 20/04/24.
+//
+
+import UIKit
+
+class MainMovieCell: UITableViewCell {
+
+    //IBOutlets
+    
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backView.addBorder(color: .label, width: 1)
+        backView.round()
+        
+        movieImageView.round(5)
+    }
+    
+
+}
